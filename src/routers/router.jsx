75df3534d9a89378a41f21deca88fc34b48d9593
Youriore <oreyouri@gmail.com>
2025-07-26 14:13:23 -0500

    noooo

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,11 +1,11 @@
-import {AppLogin} from '../pages/login.jsx'
-import {IngresosTotales} from '../pages/ingresosTotales.jsx'
-import {AlimentacionRecursos} from '../pages/alimentacionRecursos.jsx'
-import {ComidaAnimales} from '../pages/comidaAnimales.jsx'
-import {Categorias} from '../pages/categorias.jsx'
-import {CategoriaRazas} from '../pages/categoriaRazas.jsx'
-import {LoteAnimales} from '../pages/loteAnimales.jsx'
-import {ProductoAnimales} from '../pages/productoAnimales.jsx'
+import { AppLogin } from '../pages/login.jsx'
+import { IngresosTotales } from '../pages/ingresosTotales.jsx'
+import { AlimentacionRecursos } from '../pages/alimentacionRecursos.jsx'
+import { ComidaAnimales } from '../pages/comidaAnimales.jsx'
+import { Categorias } from '../pages/categorias.jsx'
+import { CategoriaRazas } from '../pages/categoriaRazas.jsx'
+import { LoteAnimales } from '../pages/loteAnimales.jsx'
+import { ProductoAnimales } from '../pages/productoAnimales.jsx'
 import { Routes, Route } from "react-router-dom";
 import { AuthRoute } from "../auth/protectedRouter";
 
@@ -13,21 +13,21 @@ export function RouterSystem() {
     return (
         <Routes>
             <Route path='/' element={<AppLogin />} />
-            <Route path='/GranjaDash/ingresosTotales' 
-            element={<AuthRoute roles={[1,2]}><IngresosTotales /></AuthRoute>} />
-            <Route path='/GranjaDash/alimentacionRecursos' 
-            element={<AuthRoute roles={[1,2]}><AlimentacionRecursos /></AuthRoute>} />
-            <Route path='/GranjaDash/comidaAnimales' 
-            element={<AuthRoute roles={[1,2]}><ComidaAnimales /></AuthRoute>} />
-            <Route path='/GranjaDash/categorias' 
-            element={<AuthRoute roles={[1,2]}><Categorias /></AuthRoute>} />
-            <Route path='/GranjaDash/categoriaRazas' 
-            element={<AuthRoute roles={[1,2]}><CategoriaRazas /></AuthRoute>} />
-            <Route path='/GranjaDash/LoteAnimales' 
-            element={<AuthRoute roles={[1,2]}><LoteAnimales /></AuthRoute>} />
-            <Route path='/GranjaDash/ProductoAnimales' 
-            element={<AuthRoute roles={[1,2]}><ProductoAnimales /></AuthRoute>} />
-            
+            <Route path='/GranjaDash/ingresosTotales'
+                element={<AuthRoute roles={[1, 2]}><IngresosTotales /></AuthRoute>} />
+            <Route path='/GranjaDash/alimentacionRecursos'
+                element={<AuthRoute roles={[1, 2]}><AlimentacionRecursos /></AuthRoute>} />
+            <Route path='/GranjaDash/comidaAnimales'
+                element={<AuthRoute roles={[1, 2]}><ComidaAnimales /></AuthRoute>} />
+            <Route path='/GranjaDash/categorias'
+                element={<AuthRoute roles={[1, 2]}><Categorias /></AuthRoute>} />
+            <Route path='/GranjaDash/categoriaRazas'
+                element={<AuthRoute roles={[1, 2]}><CategoriaRazas /></AuthRoute>} />
+            <Route path='/GranjaDash/LoteAnimales'
+                element={<AuthRoute roles={[1, 2]}><LoteAnimales /></AuthRoute>} />
+            <Route path='/GranjaDash/ProductoAnimales'
+                element={<AuthRoute roles={[1, 2]}><ProductoAnimales /></AuthRoute>} />
+
         </Routes>
     )
 }
